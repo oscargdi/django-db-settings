eaed847ddf281faaa9b6c07d0f19a61a15abb74c
Oscar Diaz <oscargdi@hotmail.com>
2020-03-13 00:31:10 -0500

    add search fields in admin view

--- settings/admin.py
+++ settings/admin.py
@@ -6,21 +6,26 @@ from .models import Setting, Field, Instance, Value
 @admin.register(Setting)
 class SettingAdmin(admin.ModelAdmin):
     list_display = ('name', 'active',)
+    search_fields = ('name',)
 
 
 @admin.register(Field)
 class FieldAdmin(admin.ModelAdmin):
     list_display = ('name', 'setting', 'public', 'active',)
     list_filter = ('setting__name',)
+    search_fields = ('setting__name', 'name',)
 
 
 @admin.register(Instance)
 class InstanceAdmin(admin.ModelAdmin):
     list_display = ('name', 'setting', 'active',)
     list_filter = ('setting__name',)
+    search_fields = ('setting__name', 'name',)
 
 
 @admin.register(Value)
 class ValueAdmin(admin.ModelAdmin):
     list_display = ('value', 'field', 'instance', )
     list_filter = ('instance__setting__name', 'instance__name', 'field__name',)
+    search_fields = ('instance__setting__name',
+                     'instance__name', 'field__name',)
