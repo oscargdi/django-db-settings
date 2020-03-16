2b6648fdf0a427a234aff253ded74966cf81cd13
Oscar Diaz <oscargdi@hotmail.com>
2020-03-15 19:08:22 -0500

    create view all template

--- settings/admin.py
+++ settings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin, messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import path
 
 from .business import clear_settings_cache
@@ -41,6 +41,8 @@ class ValueAdmin(admin.ModelAdmin):
         my_urls = [
             path('refresh/', self.admin_site.admin_view(self.refresh_view),
                  name='%s_%s_refresh' % info),
+            path('all/', self.admin_site.admin_view(self.all_view),
+                 name='%s_%s_all' % info),
         ]
         return my_urls + urls
 
@@ -52,3 +54,7 @@ class ValueAdmin(admin.ModelAdmin):
             messages.add_message(request, messages.ERROR,
                                  'Cache could not be refreshed')
         return redirect('admin:settings_value_changelist')
+
+    def all_view(self, request):
+        context = {**self.admin_site.each_context(request), }
+        return render(request, "settings/view_all_template.html", context)
